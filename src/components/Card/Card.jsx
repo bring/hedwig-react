82a2da1731cdc0ebdacaffe07ffaef3ed34aaf92
Viggo Normann <v@iggo.no>
2019-05-02 14:39:06 +0200

    Initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Card({title, imgUrl, children}) {
+    return (
+        <div className="hw-card">
+            <div className="hw-card__media">
+                <div className="hw-card__media-inner" style={{backgroundImage: `url(${imgUrl})`}}/>
+            </div>
+            <div className="hw-card__body">
+                <h3 className="hw-card__title">
+                    {title}
+                </h3>
+                <hr className="hw-hr"/>
+                <div className="hw-card__text">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+};
---
+++ src/hedwig-react.jsx
@@ -0,0 +1,3 @@
+export { default as Card } from './components/Card/Card.jsx';
+export { default as Hero } from './components/Hero/Hero.jsx';
+export { default as Spinner } from './components/Spinner/Spinner.jsx';
