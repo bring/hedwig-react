75f7082c18032d9776d0bb2752c786d535222c72
Viggo Normann <v@iggo.no>
2021-03-12 10:26:07 +0100

    Add Info Box

---
+++ src/components/InfoBox/InfoBox.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import InfoBox from './InfoBox';
+
+export default {
+  title: 'Components/Info Box',
+  component: InfoBox,
+};
+
+const Template = (args) => 
+    <InfoBox {...args} >
+        <p>The body text is here. Should not be very long</p>
+        <a className="hw-link" href="#">Click me</a>
+    </InfoBox>;
+
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'So, an Info Box'
+};
