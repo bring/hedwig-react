a521178d162a7fabff3abb6f1e0b6fe7a1c0f95c
Viggo Normann <v@iggo.no>
2021-04-09 17:35:55 +0200

    Console error if children of accordion are not of type AccordionItem

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -5,6 +5,14 @@ import { ExpandedProvider } from "./ExpandedContext";
 
 export default function Accordion({ children, allowMultiple }) {
 
+
+    children.forEach( child => {
+        if(!child.type || !child.type.displayName || child.type.displayName != 'AccordionItem') {
+            console.error('Only children of type "AccordionItem" are allowed inside an Accordion. The offending child is on the next line:');
+            console.error(child);
+        }
+    });
+
     return (
         <ul className="hw-accordion">
             <ExpandedProvider allowMultiple={ allowMultiple }>
