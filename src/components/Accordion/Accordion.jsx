2b4ee5714c4363a1af6e4c7da817ebb7d903d062
Viggo Normann <v@iggo.no>
2021-06-25 11:08:43 +0200

    Move validation of Accordion children to propTypes

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -3,19 +3,6 @@ import PropTypes from 'prop-types'
 import { ExpandedProvider } from './ExpandedContext'
 
 export default function Accordion({ children, allowMultiple, menu }) {
-    children.forEach((child) => {
-        if (
-            !child.type ||
-            !child.type.displayName ||
-            child.type.displayName !== 'AccordionItem'
-        ) {
-            console.error(
-                'Only children of type "AccordionItem" are allowed inside an Accordion. The offending child is on the next line:'
-            )
-            console.error(child)
-        }
-    })
-
     return (
         <ul className={`hw-accordion${menu ? ' hw-accordion--menu' : ''}`}>
             <ExpandedProvider allowMultiple={allowMultiple}>
@@ -27,7 +14,18 @@ export default function Accordion({ children, allowMultiple, menu }) {
 
 Accordion.propTypes = {
     allowMultiple: PropTypes.bool,
-    menu: PropTypes.bool
+    menu: PropTypes.bool,
+    children: PropTypes.arrayOf(
+        PropTypes.objectOf((child) => {
+            if (child.type.displayName !== 'AccordionItem') {
+                return new Error(
+                    'Invalid child `' +
+                        child.type.displayName +
+                        '` supplied to `Accordion`. Validation failed'
+                )
+            }
+        })
+    )
 }
 
 Accordion.defaultProps = {
