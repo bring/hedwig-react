72e65b9b60bbfb69ffcbd347092db8c12c89990b
Helle Hiorth-Schøyen <helle@solidsquare.no>
2021-08-19 12:12:40 +0200

    bugfix checkbox

--- src/components/SelectionControls/BaseCheckbox.jsx
+++ src/components/SelectionControls/BaseCheckbox.jsx
@@ -2,60 +2,65 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export default function BaseCheckbox({
-  label,
-  value,
-  checked,
-  disabled,
-  variants,
-  errorMessage,
-  required,
-  onClick
+    name,
+    label,
+    value,
+    checked,
+    disabled,
+    variants,
+    errorMessage,
+    required,
+    onClick
 }) {
-  let classes = ''
-  if (variants) {
-    variants.forEach((element) => {
-      classes += ' hw-checkbox--' + element
-    })
-  }
+    let classes = ''
+    if (variants) {
+        variants.forEach((element) => {
+            classes += ' hw-checkbox--' + element
+        })
+    }
 
-  if (errorMessage) {
-    classes += ' hw-checkbox--error'
-  }
+    if (errorMessage) {
+        classes += ' hw-checkbox--error'
+    }
 
-  return (
-    <label class={'hw-checkbox' + classes}>
-      {label}
-      <input
-        type='checkbox'
-        value={value}
-        defaultChecked={checked}
-        disabled={disabled}
-        required={required}
-        onClick={onClick}
-      />
-      <i class='hw-checkbox__indicator'></i>
-      {errorMessage && (
-        <div class='hw-error-simple hw-error--align-left'>{errorMessage}</div>
-      )}
-    </label>
-  )
+    return (
+        <label className={'hw-checkbox' + classes}>
+            {label}
+            <input
+                name={name}
+                type='checkbox'
+                value={value}
+                defaultChecked={checked}
+                disabled={disabled}
+                required={required}
+                onClick={onClick}
+            />
+            <i className='hw-checkbox__indicator' />
+            {errorMessage && (
+                <div className='hw-error-simple hw-error--align-left'>
+                    {errorMessage}
+                </div>
+            )}
+        </label>
+    )
 }
 
 BaseCheckbox.defaultProps = {
-  errorMessage: '',
-  checked: false,
-  disabled: false
+    errorMessage: '',
+    checked: false,
+    disabled: false
 }
 
 BaseCheckbox.propTypes = {
-  label: PropTypes.string,
-  value: PropTypes.string,
-  checked: PropTypes.bool,
-  disabled: PropTypes.bool,
-  variants: PropTypes.arrayOf(
-    PropTypes.oneOf(['bounding', 'full', 'horisontal'])
-  ),
-  errorMessage: PropTypes.string,
-  required: PropTypes.bool,
-  onClick: PropTypes.func
+    name: PropTypes.string,
+    label: PropTypes.string,
+    value: PropTypes.string,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    variants: PropTypes.arrayOf(
+        PropTypes.oneOf(['bounding', 'full', 'horisontal'])
+    ),
+    errorMessage: PropTypes.string,
+    required: PropTypes.bool,
+    onClick: PropTypes.func
 }
