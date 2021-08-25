94226aec1f6574ee94342ee77783b673b923db3a
Helle Hiorth-Schøyen <helle@solidsquare.no>
2021-08-25 10:47:22 +0200

    removed unused code

--- src/components/SelectionControls/BoundingCheckbox.jsx
+++ src/components/SelectionControls/BoundingCheckbox.jsx
@@ -4,48 +4,48 @@ import FieldsetContext, { FieldsetProvider } from '../Fieldset/FieldsetContext'
 import BaseCheckbox from './BaseCheckbox'
 
 export default function BoundingCheckbox({
-  name,
-  label,
-  value,
-  checked,
-  disabled,
-  errorMessage,
-  required,
-  onClick
+    name,
+    label,
+    value,
+    checked,
+    disabled,
+    errorMessage,
+    required,
+    onClick
 }) {
-  const variants = useContext(FieldsetContext)
-  let classes = ['bounding']
-  if (variants) {
-    var selectionControls = variants.find((x) => x.name === 'selectionControls')
-    selectionControls.value.forEach((element) => {
-      classes.push(element)
-    })
-  }
+    const variants = useContext(FieldsetContext)
+    let classes = ['bounding']
+    if (variants) {
+        var selectionControls = variants.find(
+            (x) => x.name === 'selectionControls'
+        )
+        selectionControls.value.forEach((element) => {
+            classes.push(element)
+        })
+    }
 
-  //classes.push('bounding')
-
-  return (
-    <BaseCheckbox
-      name={name}
-      label={label}
-      value={value}
-      checked={checked}
-      disabled={disabled}
-      variants={classes}
-      errorMessage={errorMessage}
-      required={required}
-      onClick={onClick}
-    />
-  )
+    return (
+        <BaseCheckbox
+            name={name}
+            label={label}
+            value={value}
+            checked={checked}
+            disabled={disabled}
+            variants={classes}
+            errorMessage={errorMessage}
+            required={required}
+            onClick={onClick}
+        />
+    )
 }
 
 BoundingCheckbox.propTypes = {
-  name: PropTypes.string,
-  label: PropTypes.string,
-  value: PropTypes.string,
-  checked: PropTypes.bool,
-  disabled: PropTypes.bool,
-  errorMessage: PropTypes.string,
-  required: PropTypes.bool,
-  onClick: PropTypes.func
+    name: PropTypes.string,
+    label: PropTypes.string,
+    value: PropTypes.string,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    errorMessage: PropTypes.string,
+    required: PropTypes.bool,
+    onClick: PropTypes.func
 }
