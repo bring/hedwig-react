28617e3391a849765add62b8134f210017b3bdb7
Viggo Normann <v@iggo.no>
2021-08-26 16:38:21 +0200

    Card formatting code

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,52 +1,69 @@
-import React from 'react';
+import React from 'react'
 
-function Media({imgUrl, imgFormat}) {
+function Media({ imgUrl, imgFormat }) {
     if (!imgUrl) {
-        return null;
+        return null
     }
     return (
         <div className={`hw-card__media hw-card__media-${imgFormat}`}>
-            <div className="hw-card__media-inner " style={{backgroundImage: `url(${imgUrl})`}}></div>
+            <div
+                className='hw-card__media-inner '
+                style={{ backgroundImage: `url(${imgUrl})` }}
+            ></div>
         </div>
     )
 }
 
-function Overline({overline}) {
+function Overline({ overline }) {
     if (!overline) {
-        return null;
+        return null
     }
-    return <h5 className="hw-card__overline">{overline}</h5>
+    return <h5 className='hw-card__overline'>{overline}</h5>
 }
 
-function Subtitle({subtitle}) {
+function Subtitle({ subtitle }) {
     if (!subtitle) {
-        return null;
+        return null
     }
-    return <h5 className="hw-card__subtitle">{subtitle}</h5>
+    return <h5 className='hw-card__subtitle'>{subtitle}</h5>
 }
 
-function Description({description}) {
+function Description({ description }) {
     if (!description) {
-        return null;
+        return null
     }
-    return <p className="hw-card__description">{description}</p>
+    return <p className='hw-card__description'>{description}</p>
 }
 
-export default function Card({url, title, overline, subtitle, description, compressed, imgUrl, imgFormat, target }) {
+export default function Card({
+    url,
+    title,
+    overline,
+    subtitle,
+    description,
+    compressed,
+    imgUrl,
+    imgFormat,
+    target
+}) {
     return (
-        <a className={`hw-card ${compressed ? "hw-card--compressed" : ""}`} href={url} target={target}>
+        <a
+            className={`hw-card ${compressed ? 'hw-card--compressed' : ''}`}
+            href={url}
+            target={target}
+        >
             <Media imgUrl={imgUrl} imgFormat={imgFormat} />
-            <div className="hw-card__body">
+            <div className='hw-card__body'>
                 <Overline overline={overline} />
-                <h3 className="hw-card__title">{title}</h3>
+                <h3 className='hw-card__title'>{title}</h3>
                 <Subtitle subtitle={subtitle} />
                 <Description description={description} />
             </div>
         </a>
     )
-};
+}
 
 Card.defaultProps = {
-    imgFormat: "16-9",
+    imgFormat: '16-9',
     target: false
 }
--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Card from './Card';
+import React from 'react'
+import Card from './Card'
 
 export default {
-  title: 'Components/Card',
-  component: Card,
-};
+    title: 'Components/Card',
+    component: Card
+}
 
-const Template = (args) => <Card {...args} />;
+const Template = (args) => <Card {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
-  title: 'So, a card',
-  imgUrl: 'http://placekitten.com/201/300'
-};
+    title: 'So, a card',
+    imgUrl: 'http://placekitten.com/201/300'
+}
