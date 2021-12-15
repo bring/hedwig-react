9d1ae64e533829b969850eab8064947df213f9aa
Helle Hiorth-Schøyen <helle@solidsquare.no>
2021-12-15 12:50:49 +0100

    bugfix radiobuttons missing export

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ export { default as PasswordInput } from './components/Input/PasswordInput.jsx'
 export { default as Checkbox } from './components/SelectionControls/Checkbox.jsx'
 export { default as BoundingCheckbox } from './components/SelectionControls/BoundingCheckbox.jsx'
 
+export { default as RadioButton } from './components/SelectionControls/RadioButton.jsx'
+export { default as BoundingRadioButton } from './components/SelectionControls/BoundingRadioButton.jsx'
+
 export { default as InfoBox } from './components/InfoBox/InfoBox.jsx'
 export { default as WarningBox } from './components/WarningSubtle/WarningSubtle.jsx'
 export { default as InfoSubtle } from './components/InfoSubtle/InfoSubtle.jsx'
