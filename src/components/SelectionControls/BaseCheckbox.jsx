cd04f1bf4e4069f7b281ebc30e5f43143b672c1e
Helle Hiorth-Schøyen <helle@solidsquare.no>
2022-02-22 16:59:35 +0100

    some fixes to checkbox (#68)

--- src/components/SelectionControls/BaseCheckbox.jsx
+++ src/components/SelectionControls/BaseCheckbox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function BaseCheckbox({
@@ -43,7 +43,6 @@ export default function BaseCheckbox({
                     {errorMessage}
                 </div>
             )}
-
         </>
     )
 }
--- src/components/SelectionControls/Checkbox.jsx
+++ src/components/SelectionControls/Checkbox.jsx
@@ -1,54 +1,56 @@
 import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
-import FieldsetContext, { FieldsetProvider } from '../Fieldset/FieldsetContext'
+import FieldsetContext from '../Fieldset/FieldsetContext'
 import BaseCheckbox from './BaseCheckbox'
 
 export default function Checkbox({
-  name,
-  label,
-  value,
-  checked,
-  disabled,
-  errorMessage,
-  required,
-  onClick
+    name,
+    label,
+    value,
+    checked,
+    disabled,
+    errorMessage,
+    required,
+    onClick
 }) {
-  const variants = useContext(FieldsetContext)
-  let classes = []
-  if (variants) {
-    var selectionControls = variants.find((x) => x.name === 'selectionControls')
-    selectionControls.value.forEach((element) => {
-      classes.push(element)
-    })
-  }
+    const variants = useContext(FieldsetContext)
+    const classes = []
+    if (variants) {
+        const selectionControls = variants.find(
+            (x) => x.name === 'selectionControls'
+        )
+        selectionControls.value.forEach((element) => {
+            classes.push(element)
+        })
+    }
 
-  return (
-    <BaseCheckbox
-      name={name}
-      label={label}
-      value={value}
-      checked={checked}
-      disabled={disabled}
-      variants={classes}
-      errorMessage={errorMessage}
-      required={required}
-      onClick={onClick}
-    />
-  )
+    return (
+        <BaseCheckbox
+            name={name}
+            label={label}
+            value={value}
+            checked={checked}
+            disabled={disabled}
+            variants={classes}
+            errorMessage={errorMessage}
+            required={required}
+            onClick={onClick}
+        />
+    )
 }
 
 Checkbox.defaultProps = {
-  errorMessage: '',
-  checked: false
+    errorMessage: '',
+    checked: false
 }
 
 Checkbox.propTypes = {
-  name: PropTypes.string,
-  label: PropTypes.string,
-  value: PropTypes.string,
-  checked: PropTypes.bool,
-  disabled: PropTypes.bool,
-  errorMessage: PropTypes.string,
-  required: PropTypes.bool,
-  onClick: PropTypes.func
+    name: PropTypes.string,
+    label: PropTypes.string,
+    value: PropTypes.string,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    errorMessage: PropTypes.string,
+    required: PropTypes.bool,
+    onClick: PropTypes.func
 }
