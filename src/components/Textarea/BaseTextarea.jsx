2366337135868516fbc3cce023143e9038522f4d
Helle Hiorth-Schøyen <helle@solidsquare.no>
2022-03-25 12:49:46 +0100

    bugfix and formatting for textarea (#82)

--- src/components/Textarea/BaseTextarea.jsx
+++ src/components/Textarea/BaseTextarea.jsx
@@ -2,71 +2,68 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function BaseTextarea({
-  id,
-  name,
-  label,
-  variant,
-  placeholder,
-  rows,
-  cols,
-  maxLength,
-  onChange,
-  disabled,
-  readOnly,
-  errorMessage,
-  children
+    id,
+    name,
+    label,
+    variant,
+    placeholder,
+    rows,
+    cols,
+    maxLength,
+    onChange,
+    disabled,
+    readOnly,
+    errorMessage,
+    children
 }) {
-  let labelVariant = variant == 'line' ? 'hw-label--line' : ''
-  let variation = variant != '' ? 'hw-textarea--' + variant : ''
-  if (errorMessage != '') {
-      labelVariant += ' hw-label--error'
-      variation += ' hw-textarea--error'
-  }
-   
-  
- 
+    let labelVariant = variant === 'line' ? 'hw-label--line' : ''
+    let variation = variant !== '' ? 'hw-textarea--' + variant : ''
+    if (errorMessage !== '') {
+        labelVariant += ' hw-label--error'
+        variation += ' hw-textarea--error'
+    }
 
-  return (
-    <label className={'hw-label ' + labelVariant} >
-      {label}
-      <textarea
-        id={id}
-        name={name}
-        className={'hw-textarea ' + variation}
-        rows={rows}
-        cols={cols}
-        maxlength={maxLength}
-        placeholder={placeholder}
-        onChange={onChange}
-        disabled={disabled}
-        readOnly={readOnly}
-      >
-        {children}
-      </textarea>
-      {errorMessage && (
-        <div class='hw-error hw-error--align-left'>
-          {errorMessage}
-        </div>
-      )}
-    </label>
-  )
+    return (
+        <label className={'hw-label ' + labelVariant}>
+            {label}
+            <textarea
+                id={id}
+                name={name}
+                className={'hw-textarea ' + variation}
+                rows={rows}
+                cols={cols}
+                maxLength={maxLength}
+                placeholder={placeholder}
+                onChange={onChange}
+                disabled={disabled}
+                readOnly={readOnly}
+            >
+                {children}
+            </textarea>
+            {errorMessage && (
+                <div className='hw-error hw-error--align-left'>
+                    {errorMessage}
+                </div>
+            )}
+        </label>
+    )
 }
 
 BaseTextarea.defaultProps = {
-    variant : '',
-    errorMessage : ''
+    variant: '',
+    errorMessage: ''
 }
 
-BaseTextarea.PropTypes = {
-  name: PropTypes.string,
-  label: PropTypes.string,
-  variant: PropTypes.oneOf(['white', 'line', '']),
-  placeholder: PropTypes.string,
-  rows: PropTypes.number,
-  cols: PropTypes.number,
-  maxLength: PropTypes.number,
-  onChange: PropTypes.func,
-  disabled: PropTypes.bool,
-  readOnly: PropTypes.bool,
-  errorMessage: PropTypes.string
+BaseTextarea.propTypes = {
+    name: PropTypes.string,
+    label: PropTypes.string,
+    variant: PropTypes.oneOf(['white', 'line', '']),
+    placeholder: PropTypes.string,
+    rows: PropTypes.number,
+    cols: PropTypes.number,
+    maxLength: PropTypes.number,
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
+    readOnly: PropTypes.bool,
+    errorMessage: PropTypes.string
 }
